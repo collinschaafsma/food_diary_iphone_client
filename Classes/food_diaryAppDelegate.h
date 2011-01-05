a3102d17dacfbd0b30db285757b916d629a1ea05
Collin Schaafsma <collin@quickleft.com>
2011-01-04 19:22:05 -0700

    initial import of phone gap app

---
+++ Classes/food_diaryAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  food_diaryAppDelegate.h
+//  food_diary
+//
+//  Created by Collin Schaafsma on 1/4/11.
+//  Copyright Quick Left 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface food_diaryAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  food_diary
+//
+//  Created by Collin Schaafsma on 1/4/11.
+//  Copyright Quick Left 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"food_diaryAppDelegate");
+    [pool release];
+    return retVal;
+}
